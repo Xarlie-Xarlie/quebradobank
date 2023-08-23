e9eee7ccdaf313454798c920d7bc7e78d0acbe45
charliecharlie <jeancharles552@gmail.com>
2023-08-23 01:33:50 -0300

    fix: remove welcome controller

--- lib/quebrado_bank_web/controllers/welcome_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule QuebradoBankWeb.WelcomeController do
-  use QuebradoBankWeb, :controller
-
-  def index(conn, _params) do
-    conn
-    |> put_status(:ok)
-    |> json(%{message: "Welcome to Quebrado Bank", status: :ok})
-  end
-end
